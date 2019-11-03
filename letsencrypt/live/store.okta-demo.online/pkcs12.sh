104bfcf358948a33178d817e92ec1c223b7ebb7e
Wasim Alayoubi <wasim.alayoubi@mail.concordia.ca>
2019-11-03 14:54:03 -0500

    enabled https

---
+++ letsencrypt/live/store.okta-demo.online/pkcs12.sh
@@ -0,0 +1,1 @@
+openssl pkcs12 -export -out store.okta-demo.online.p12 -inkey privkey.pem  -in cert.pem -certfile chain.pem
--- src/main/java/com/okta/developer/oidc/OidcApplication.java
+++ src/main/java/com/okta/developer/oidc/OidcApplication.java
@@ -1,5 +1,8 @@
 package com.okta.developer.oidc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OidcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OidcApplication.class, args);
+		SpringApplication app = new SpringApplication(OidcApplication.class);
+		Map<String, Object> configuration = new HashMap<>();
+		configuration.put("server.port", "443");
+		configuration.put("server.ssl.key-store", "resources/store.okta-demo.online.jks");
+		configuration.put("server.ssl.keyStoreType", "jks");
+		configuration.put("server.ssl.keyAlias", "store.okta-demo.online");
+		configuration.put("server.ssl.key-store-password", "changeit");
+		
+		
+		
+		app.setDefaultProperties(configuration);
+		app.run(args);
 	}
 }
