fb8fea5674193978ed3d1685b99b3a2e36fe0231
Wasim Alayoubi <wasim.alayoubi@mail.concordia.ca>
2019-11-22 11:35:47 -0500

    updated script

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 git pull
+
+mv src/main/resources/application.yml src/main/resources/application.yml.dev
+
+mv src/main/resources/application.yml.live src/main/resources/application.yml 
+
 ./mvnw compile spring-boot:run
